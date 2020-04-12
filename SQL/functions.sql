b96d93839eb35c950fc511723668937817bf5894
Nagasuye <60358155+Nagasuye@users.noreply.github.com>
2020-04-12 19:28:15 -0400

    Add files via upload

--- SQL/functions.sql
+++ SQL/functions.sql
@@ -1,3 +1,5 @@
+--checks if the given username and password is in the database
+--if they are, the users information is returned
 function checkLogin(usrn, pswd){
 	return new Promise(function(resolve){
 		client.query('select * from person where username = $1 and password = $2', [usrn, pswd], (err, results)=>{
@@ -7,6 +9,7 @@ function checkLogin(usrn, pswd){
 	});
 }
 
+--gets the orders of the given user
 function getOrders(sesh){
 	if(sesh.owner){
 		return new Promise(function(resolve){
@@ -25,6 +28,7 @@ function getOrders(sesh){
 	}
 }
 
+--adds the given publisher to the database
 function addPublisher(nam, stnum, stn, email, phnum){
 	return new Promise(function(resolve){
 		client.query('insert into publisher (publisher_name, street_num, street_name, email, phone_num) values ($1, $2, $3, $4, $5)', [nam, stnum, stn, email, phnum], (err, results)=>{
@@ -34,6 +38,7 @@ function addPublisher(nam, stnum, stn, email, phnum){
 	});
 }
 
+--adds the given author to the database
 function addAuthor(nam, age){
 	return new Promise(function(resolve){
 		client.query('insert into author (author_name, age) values ($1, $2)', [nam, age], (err, results)=>{
@@ -43,6 +48,7 @@ function addAuthor(nam, age){
 	});
 }
 
+--adds the given book to the database
 function addBook(nam, anam, pnam, wid, yr, isbn, page, price, gen){
 	return new Promise(function(resolve){
 		client.query('insert into book (name, author_name, publisher_name, warehouse_id, year, ISBN, pages, price, genre) values ($1, $2, $3, $4, $5, $6, $7, $8, $9)', [nam, anam, pnam, wid, yr, isbn, page, price, gen], (err, results)=>{
@@ -52,6 +58,7 @@ function addBook(nam, anam, pnam, wid, yr, isbn, page, price, gen){
 	});
 }
 
+--adds the given address to the database
 function addAddress(stno, stn, city, pc){
 	return new Promise(function(resolve){
 		client.query('insert into address (street_num, street_name, city, postal_code) values ($1, $2, $3, $4)', [stno, stn, city, pc], (err, results)=>{
@@ -61,6 +68,7 @@ function addAddress(stno, stn, city, pc){
 	});
 }
 
+--adds the given user to the database after the user submits registration info
 function addUser(un, nam, age, pswd, stno, stn, city, pc){
 	return new Promise(function(resolve){
 		let addressAdded = addAddress(stno, stn, city, pc);
@@ -73,6 +81,7 @@ function addUser(un, nam, age, pswd, stno, stn, city, pc){
 	});
 }
 
+--finds the book matching the given name
 function findBook(name){
 	return new Promise(function(resolve){
 		client.query('select * from book where name = $1', [name], (err, results)=>{
@@ -82,6 +91,7 @@ function findBook(name){
 	});
 }
 
+--searched the books that match the given category's term
 function searchBooks(term, cat){
 	if(cat == 0){
 		return new Promise(function(resolve){
@@ -115,6 +125,7 @@ function searchBooks(term, cat){
 	
 }
 
+--removed book from database that matches the given name
 function removeBook(nam){
 	return new Promise(function(resolve){
 		client.query('delete from book where name = $1', [nam], (err, results)=>{
@@ -124,6 +135,7 @@ function removeBook(nam){
 	});
 }
 
+--adds the given order details to the database as an order
 function addOrder(nam, bankNum, bankName){
 	return new Promise(function(resolve){
 		client.query('insert into purchase (username, bank_num, bank_name) values ($1, $2, $3)', [nam, bankNum, bankName], (err, results)=>{
@@ -133,6 +145,7 @@ function addOrder(nam, bankNum, bankName){
 	});
 }
 
+--adds the given tracking to the database
 function addTracking(nam, stNo, stNa){
 	return new Promise(function(resolve){
 		client.query('insert into tracking (username, street_num, street_name) values ($1, $2, $3)', [nam, stNo, stNa], (err, results)=>{
@@ -142,6 +155,7 @@ function addTracking(nam, stNo, stNa){
 	});
 }
 
+--adds the given book orders to the database (bks is an array of book orders)
 function addBookOrders(bks){
 	let inserts = bks.map(item=>"('" + item.name + "', '" + item.username + "', '" + item.quantity + "')" );
 	return new Promise(function(resolve){
@@ -152,6 +166,7 @@ function addBookOrders(bks){
 	});
 }
 
+--returns all the inventory currently in the book store
 function getInventory(){
 	return new Promise(function(resolve){
 		client.query('select * from book', (err, results)=>{
